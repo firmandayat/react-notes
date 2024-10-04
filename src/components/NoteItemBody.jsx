78abf2d5a88e362bb9293945469d69982d3a6b4b
firmandayat <firman.txt@gmail.com>
2024-10-04 20:54:41 +0700

    ganti nama komponen NoteItemBody

--- src/components/NoteItemBody.jsx
+++ src/components/NoteItemBody.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function NotesItemBody({title, createdAt ,body}){
+function NoteItemBody({title, createdAt ,body}){
     return(
         <div className="note-item__body">
             <h3 className="note-item__title">{title}</h3>
@@ -10,4 +10,4 @@ function NotesItemBody({title, createdAt ,body}){
     )
 }
 
-export default NotesItemBody
+export default NoteItemBody
