3887b29612f7355c056422719e8d3dcc7746916d
firmandayat <firman.txt@gmail.com>
2024-10-06 16:19:19 +0700

    add icons and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <div className="footer">
+            <footer>
+                <p>&copy; My Notes All rights reserved 2024</p>
+                <p>Firman Hidayatullah</p>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
