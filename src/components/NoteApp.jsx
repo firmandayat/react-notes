c008d6f340b47a06a0c23e412cc11d7f13bc2a53
firmandayat <firman.txt@gmail.com>
2024-10-06 12:37:39 +0700

    search style button

--- src/components/NoteApp.jsx
+++ src/components/NoteApp.jsx
@@ -7,11 +7,13 @@ class NoteApp extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            notes: getInitialData()
+            notes: getInitialData(),
+            searchNote: ''
         }
         this.onDeleteHandler = this.onDeleteHandler.bind(this)
         this.onAddNoteHandler = this.onAddNoteHandler.bind(this)
         this.onArchiveHandler = this.onArchiveHandler.bind(this);
+        this.onSearchHandler = this.onSearchHandler.bind(this);
     }
 
     onDeleteHandler(id){
@@ -46,9 +48,21 @@ class NoteApp extends React.Component{
         this.setState({ notes });
     }
 
+    onSearchHandler(event){
+         this.setState({ searchNote: event.target.value });
+    }
+
     render(){
         const activeNotes = this.state.notes.filter((note) => !note.archived);
         const archivedNotes = this.state.notes.filter((note) => note.archived);
+
+        const filteredActiveNotes = activeNotes.filter(note =>
+            note.title.toLowerCase().includes(this.state.searchNote.toLowerCase())
+        );
+
+        const filteredArchivedNotes = archivedNotes.filter(note =>
+            note.title.toLowerCase().includes(this.state.searchNote.toLowerCase())
+        );
 
         return(
               <div className="note-app">
@@ -56,11 +70,13 @@ class NoteApp extends React.Component{
               <h3 className="note-app__title">Add Note</h3>
               <NoteInput addNote={this.onAddNoteHandler}/>
 
+              <input className="note-app__search" type="text" placeholder="Search notes..." value={this.state.searchNote} onChange={this.onSearchHandler}/>
+
               <h3 className="note-app__title-item">Active Notes</h3>
-              <NoteList notes={activeNotes} onDelete={this.onDeleteHandler} onArchived={this.onArchiveHandler} />
+              <NoteList notes={filteredActiveNotes} onDelete={this.onDeleteHandler} onArchived={this.onArchiveHandler} />
               <br /><br />
               <h3 className="note-app__title-item">Archived Notes</h3>
-              <NoteList notes={archivedNotes} onDelete={this.onDeleteHandler} onArchived={this.onArchiveHandler} />
+              <NoteList notes={filteredArchivedNotes} onDelete={this.onDeleteHandler} onArchived={this.onArchiveHandler} />
 
               {/* <NoteList notes={this.state.notes} onDelete={this.onDeleteHandler}/> */}
               </div>
